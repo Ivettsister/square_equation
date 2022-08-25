f935c9350bb08899c80e15a21c3e9ff5170dc67b
Ivettsister <ivettsister@yandex.ru>
2022-08-25 19:51:33 +0300

    add makefile

--- common.cpp
+++ common.cpp
@@ -3,20 +3,20 @@
 const double Eps = 1E-6;
 
 void clear_stdin() {
-   while (getchar() != '\n');
+    while (getchar() != '\n');
 }
 
 int is_equal(const float a, const float b) {
-   assert(!isnan(a));
-   assert(!isnan(b));
+    assert(!isnan(a));
+    assert(!isnan(b));
 
-   return compare_difference(a, b, Eps);
+    return compare_difference(a, b, Eps);
 }
 
 int compare_difference(const float a, const float b, const double eps) {
-   assert(!isnan(a));
-   assert(!isnan(b));
-   assert(!isnan(eps));
+	assert(!isnan(a));
+    assert(!isnan(b));
+    assert(!isnan(eps));
    
-   return abs(b - a) < eps;
+    return abs(b - a) < eps;
 }
